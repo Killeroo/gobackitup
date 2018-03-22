ec56d6bdf5eb9eb362287a6167b8e33113708515
Matthew Carney <matthewcarney64@gmail.com>
2018-03-22 18:11:18 +0000

    Added custom name Added folder creation to CopyFile

--- src/gobackitup.go
+++ src/gobackitup.go
@@ -1,4 +1,9 @@
 // Todo: Use goroutines for copy contents of files 
+// Todo: Move backup details to a struct?
+// Zipping:
+// http://blog.ralch.com/tutorial/golang-working-with-zip/
+// https://gist.github.com/svett/424e6784facc0ba907ae
+// https://golang.org/src/archive/zip/example_test.go
 
 package main
 
@@ -14,6 +19,7 @@ import (
 var backupDst string
 var backupSrc string
 var backupName string
+var backupZip bool
 
 // Called by filepath.Walk() whenever it comes accross a file, 
 // determine its new path at the destination and copy over it over
@@ -34,6 +40,8 @@ func handle(path string, f os.FileInfo, err error) error {
 
 // First check the source file is non regular (directory, symlink etc)
 // all these checks are performed using os.Stat which returns fileinfo.
+// If the source is a folder then a corresponding folder is created in
+// the destination.
 // Secondly check the destination file is non regular AND that it isnt the
 // same in as the source file
 // Else copy the contents of the source file to the new destination file
@@ -44,7 +52,12 @@ func copyFile(src, dst string) (err error) {
 		return
 	}
 	if !sfi.Mode().IsRegular() {
-		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
+		if sfi.Mode().IsDir() {
+			os.MkdirAll(dst, os.ModePerm)
+			return
+		} else {
+			return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())		
+		}
 	}
 
 	dfi, err := os.Stat(dst)
@@ -74,10 +87,6 @@ func copyFileContents(src, dst string) (err error) {
 	}
 	defer in.Close()
 
-	path, _ := filepath.Split(dst)
-	if  _, err := os.Stat(path); os.IsNotExist(err) {
-		os.MkdirAll(path, os.ModePerm)
-	}
 	out, err := os.Create(dst)
 	if err != nil {
 		return
@@ -99,11 +108,13 @@ func copyFileContents(src, dst string) (err error) {
 func init() {
 	// Setup arguments/flags
 	flag.StringVar(&backupSrc, "source", "", "Path to backup")
-	flag.StringVar(&backupSrc, "s", "",  "Path to backup (shorthand)")
+	flag.StringVar(&backupSrc, "s", "",  "(shorthand) Path to backup")
 	flag.StringVar(&backupDst, "destination", "", "Path to save backup to")
-	flag.StringVar(&backupDst, "d", "", "Path to save backup to (shorthand)")
-	flag.StringVar(&backupName, "name", "", "Name of folder to save backup to (optional)")
-	flag.StringVar(&backupName, "n", "", "Name of folder to save backup to (optional) (shorthand)")
+	flag.StringVar(&backupDst, "d", "", "(shorthand) Path to save backup to")
+	flag.StringVar(&backupName, "name", "", "(optional) Name of folder to save backup to ")
+	flag.StringVar(&backupName, "n", "", "(optional) (shorthand) Name of folder to save backup to")
+	flag.BoolVar(&backupZip, "zip", false, "(optional) Compress backup")
+	flag.BoolVar(&backupZip, "z", false, "(optional) (shorthand) Compress backup")
 }
 
 func main() {
@@ -114,6 +125,14 @@ func main() {
 		fmt.Fprint(os.Stderr, "Please specify a source and a destination path for the backup\n")
 		flag.Usage()
 		os.Exit(1)
+	}
+
+	if backupName != "" {
+		backupDst = filepath.Join(backupDst, backupName)
+		if err := os.MkdirAll(backupDst, os.ModePerm); err != nil {
+			fmt.Fprint(os.Stderr, "Could not create folder %s", backupName)
+			os.Exit(1)
+		}
 	}
 	
 	err := filepath.Walk(backupSrc, handle)
